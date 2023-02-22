8af75820b54c6d56d0b28c47a9ae317f37948fd6
Tim.Dang <tim.dang@pal.net.vn>
2023-02-22 14:16:54 +0700

    Implement TCs of EventMarketLogPage

--- src/main/java/backoffice/testcases/operations/MixedPTConfigurationTest.java
+++ src/main/java/backoffice/testcases/operations/MixedPTConfigurationTest.java
@@ -12,12 +12,12 @@ public class MixedPTConfigurationTest extends BaseCaseTest {
 
     @TestRails(id = "1649")
     @Test (groups = {"regression"})
-    public void BO_Operations_Announcement_Management_1649(){
+    public void BO_Operations_MixedPT_Configuration_1649(){
         log("@title: Validate can search and navigate to page correctly");
         log("Step 1. Access Operations > Mixed-PT Configuration");
         MixedPTConfigurationPage page = backofficeHomePage.navigateMixedPTConfiguration();
 
-        log("Verify page title displays with Announcement Management");
+        log("Verify page title displays with Mixed-PT Configuration");
         Assert.assertEquals("Mixed-PT Configuration", page.lblPageTitle.getText(), "FAILED ! Page title is not displayed correctly, actual: " + page.lblPageTitle.getText());
 
         log("INFO: Executed completely");
@@ -25,8 +25,8 @@ public class MixedPTConfigurationTest extends BaseCaseTest {
 
     @TestRails(id = "1650")
     @Test (groups = {"regression"})
-    public void BO_Operations_Announcement_Management_1650(){
-        log("@title: Validate can search and navigate to page correctly");
+    public void BO_Operations_MixedPT_Configuration_1650(){
+        log("@title: Validate can filter");
         log("Step 1. Access Operations > Mixed-PT Configuration");
         MixedPTConfigurationPage page = backofficeHomePage.navigateMixedPTConfiguration();
 
