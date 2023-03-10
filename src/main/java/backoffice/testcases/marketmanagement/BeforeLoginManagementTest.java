ad4fe84295d9c87976bbbbbb5602cb0e1d132101
Tim.Dang <tim.dang@pal.net.vn>
2023-03-10 11:45:40 +0700

    Implement TCs of BeforeLoginManagementPage

--- src/main/java/backoffice/testcases/marketmanagement/BeforeLoginManagementTest.java
+++ src/main/java/backoffice/testcases/marketmanagement/BeforeLoginManagementTest.java
@@ -1,6 +1,9 @@
 package backoffice.testcases.marketmanagement;
 
-import baseTest.BaseCaseMerito;
+import backoffice.common.BOConstants;
+import backoffice.pages.bo.marketmanagement.components.BeforeLoginManagementPopup;
+import backoffice.utils.system.CurrencyManagementUtils;
+import baseTest.BaseCaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -8,9 +11,10 @@ import backoffice.pages.bo._components.AlertMessageBox;
 import backoffice.pages.bo.marketmanagement.BeforeLoginManagementPage;
 import util.testraildemo.TestRails;
 
+import java.util.Comparator;
 import java.util.List;
 
-public class BeforeLoginManagementTest extends BaseCaseMerito {
+public class BeforeLoginManagementTest extends BaseCaseTest {
 
     /**
      * @title: Validate sport is active/deactivate effect to member site before login page
@@ -20,7 +24,7 @@ public class BeforeLoginManagementTest extends BaseCaseMerito {
      *      2. Active/Deactive a sport
      * @expect: 1. Verify the sport display/not display in member site
      */
-    @Test (groups = {"regression"})
+    @Test (groups = {"regression_invalid"})
     @Parameters({"username","password"})
     public void BO_Operations_Before_Login_Management_001(String username, String password) throws Exception {
         /*log("@title: Validate sport is active/deactivate effect to member site before login page");
@@ -84,7 +88,7 @@ public class BeforeLoginManagementTest extends BaseCaseMerito {
      *          3. Active and inactive a  market:  Tied Match
      * @expect: 1. Verify the market is active/inactive on before login member. In the left menu, select cricket and any competition, verify Tied Match market is display/disappear per setting
      */
-    @Test (groups = {"regression"})
+    @Test (groups = {"regression_invalid"})
     @Parameters({"username","password"})
     public void BO_Operations_Before_Login_Management_002(String username, String password) throws Exception {
         log("@title: Validate sport is active/deactivate effect to member site before login page");
@@ -181,5 +185,190 @@ public class BeforeLoginManagementTest extends BaseCaseMerito {
         List<String> lstMarket = page.tblMarketType.getColumn(page.colMarketType,false);
         Assert.assertTrue(lstMarket.contains("Fancy"),"The list market does not contains search key");
 
+    }
+    @TestRails(id = "1653")
+    @Test (groups = {"regression"})
+    public void BO_Operations_Before_Login_Management_1653(){
+        log("@title: Validate can search and navigate to page correctly");
+        log("Step 1. Access Operations > Before Login Management");
+        BeforeLoginManagementPage page = backofficeHomePage.navigateBeforeLoginManagement();
+
+        log("Step 1. Verify page title displays Before Login Management");
+        Assert.assertEquals("Before Login Management", page.lblPageTitle.getText(), "FAILED ! Page title is not displayed correctly, actual: " + page.lblPageTitle.getText());
+        log("INFO: Executed completely");
+    }
+
+    @TestRails(id = "1654")
+    @Test (groups = {"regression"})
+    public void BO_Operations_Before_Login_Management_1654(){
+        String type = "Header Menu";
+        log("@title: Validate can switch type Header Menu");
+        log("Step 1. Access Operations > Before Login Management");
+        BeforeLoginManagementPage page = backofficeHomePage.navigateBeforeLoginManagement();
+
+        log("Step 2. Filter with type = Header Menu");
+        page.filter(type,"FairExchange","","",false);
+        log("Step 1. Verify page UI displays correctly with type = Header Menu");
+        Assert.assertTrue(page.isUIDisplayCorrect(type), "FAILED ! Page UI is not displayed correctly for type: " + type);
+
+        log("INFO: Executed completely");
+    }
+
+    @TestRails(id = "1655")
+    @Test (groups = {"regression"})
+    public void BO_Operations_Before_Login_Management_1655(){
+        String type = "Sport & Market Type";
+        log("@title: Validate can switch type Sport & Market Type");
+        log("Step 1. Access Operations > Before Login Management");
+        BeforeLoginManagementPage page = backofficeHomePage.navigateBeforeLoginManagement();
+
+        log("Step 2. Filter with type = Sport & Market Type");
+        page.filter(type,"FairExchange","","",false);
+        log("Step 1. Verify page UI displays correctly with type = Sport & Market Type");
+        Assert.assertTrue(page.isUIDisplayCorrect(type), "FAILED ! Page UI is not displayed correctly for type: " + type);
+
+        log("INFO: Executed completely");
+    }
+
+    @TestRails(id = "1656")
+    @Test (groups = {"regression"})
+    public void BO_Operations_Before_Login_Management_1656(){
+        String type = "Header Menu";
+        log("@title: Validate UI of Create Header Menu");
+        log("Step 1. Access Operations > Before Login Management");
+        BeforeLoginManagementPage page = backofficeHomePage.navigateBeforeLoginManagement();
+
+        log("Step 2. Filter with type = Header Menu");
+        page.filter(type,"FairExchange","","",false);
+        log("Step 2. Click Create Menu button");
+        BeforeLoginManagementPopup popup = page.openCreateHeaderMenu();
+        log("Verify UI in Create Header Menu popup with Sub-Menu = No shows correctly");
+        Assert.assertTrue(popup.isUICreateMenuDisplayCorrect(false),"FAILED ! Page UI is not displayed correctly for Sub Menu = No");
+        popup.ddpSubMenu.selectByVisibleText("Yes");
+        log("Verify UI in Create Header Menu popup with Sub-Menu = Yes shows correctly");
+        Assert.assertTrue(popup.isUICreateMenuDisplayCorrect(true),"FAILED ! Page UI is not displayed correctly for Sub Menu = Yes");
+
+        log("INFO: Executed completely");
+    }
+
+    @TestRails(id = "1657")
+    @Test (groups = {"regression"})
+    @Parameters({"feMemberLoginId","feMemberLoginPwd","language","currency"})
+    public void BO_Operations_Before_Login_Management_1657(String feMemberLoginId, String feMemberLoginPwd, String language, String currency) throws Exception {
+        String type = "Header Menu";
+        log("@title: Validate FairExchange Before Login - Header Menu show as setting");
+        log("Step 1. Access Operations > Before Login Management");
+        BeforeLoginManagementPage page = backofficeHomePage.navigateBeforeLoginManagement();
+
+        log("Step 2. Filter with type = Header Menu for FairExchange");
+        page.filter(type,"FairExchange","", BOConstants.Operations.BannerManagement.POPUP_DDB_STATUS.get(0),true);
+        String domain = page.ddpDomain.getFirstSelectedOption();
+
+        log("Step 3. Get list active menu and sequence");
+        List<List<String>> lstMenuHeader = page.getListMenuSequence();
+        List<String> lstMenu = lstMenuHeader.get(0);
+        List<String> lstSequence = lstMenuHeader.get(1);
+        lstMenu.sort(Comparator.comparingInt(lstSequence::indexOf));
+        log("Step 4. Login to member site before login");
+        memberLoginURL = "https://"+ domain + "/plus";
+        loginMember(feMemberLoginId, feMemberLoginPwd, false, language, currency, false);
+
+        log("Verify. Validate header menu show with active from BO with correct sequence");
+        List<String> lstHeaderMenuLogin = landingPage.getListHeaderMenu();
+        for (int i = 0;i < lstMenu.size(); i++) {
+            Assert.assertTrue(lstMenu.get(i).equalsIgnoreCase(lstHeaderMenuLogin.get(i+1)),"FAILED! Header menu is not displayed correctly follow BO site");
+        }
+
+        log("INFO: Executed completely");
+    }
+
+    @TestRails(id = "1658")
+    @Test (groups = {"regression"})
+    @Parameters({"feMemberLoginId","feMemberLoginPwd","language","currency"})
+    public void BO_Operations_Before_Login_Management_1658(String feMemberLoginId, String feMemberLoginPwd, String language, String currency) throws Exception {
+        String type = "Header Menu";
+        log("@title: Validate FairExchange Before Login - Header Menu show as setting");
+        log("Step 1. Access Operations > Before Login Management");
+        BeforeLoginManagementPage page = backofficeHomePage.navigateBeforeLoginManagement();
+
+        log("Step 2. Filter with type = Header Menu for FairExchange");
+        page.filter(type,"FairExchange","", BOConstants.Operations.BannerManagement.POPUP_DDB_STATUS.get(0),true);
+        String domain = page.ddpDomain.getFirstSelectedOption();
+
+        log("Step 3. Get list active menu and sequence");
+        List<List<String>> lstMenuHeader = page.getListMenuSequence();
+        List<String> lstMenu = lstMenuHeader.get(0);
+        List<String> lstSequence = lstMenuHeader.get(1);
+        lstMenu.sort(Comparator.comparingInt(lstSequence::indexOf));
+        log("Step 4. Login to member site before login");
+        memberLoginURL = "https://"+ domain + "/plus";
+        memberSOSUrl = "https://"+ domain;
+        loginMember(feMemberLoginId, feMemberLoginPwd, true, language, currency, true);
+
+        log("Verify. Validate header menu show with active from BO with correct sequence");
+        List<String> lstHeaderMenuLogin = memberHomePage.getListHeaderMenu();
+        for (int i = 0;i < lstMenu.size(); i++) {
+            Assert.assertTrue(lstMenu.get(i).equalsIgnoreCase(lstHeaderMenuLogin.get(i+1)),"FAILED! Header menu is not displayed correctly follow BO site");
+        }
+
+        log("INFO: Executed completely");
+    }
+
+    @TestRails(id = "1659")
+    @Test (groups = {"regression1"})
+    @Parameters({"feMemberLoginId","feMemberLoginPwd","language","currency"})
+    public void BO_Operations_Before_Login_Management_1659(String feMemberLoginId, String feMemberLoginPwd, String language, String currency) throws Exception {
+        Float rate;
+        String type = "Header Menu";
+        log("@title: Validate Currency show correctly");
+        log("Step 1. Access Operations > Before Login Management");
+        BeforeLoginManagementPage page = backofficeHomePage.navigateBeforeLoginManagement();
+
+        log("Step 2. Filter with type = Header Menu for FairExchange");
+        page.filter(type,"FairExchange","", BOConstants.Operations.BannerManagement.POPUP_DDB_STATUS.get(0),true);
+        String domain = page.ddpDomain.getFirstSelectedOption();
+        log("Step 3. Get currency of current branch filter");
+        String currencySelected = page.ddpCurrency.getFirstSelectedOption();
+        rate = CurrencyManagementUtils.getCurrencyRate(currencySelected);
+        memberLoginURL = "https://"+ domain + "/plus";
+        memberSOSUrl = "https://"+ domain;
+        log("Step 4. Login to member site");
+        loginMember(feMemberLoginId, feMemberLoginPwd, false, language, currency, true);
+        List<String> lst = landingPage.getBeforeLoginConfig(memberSOSUrl);
+
+        log("Verify. Validate currency between BO site and Member site before login matched");
+        Assert.assertEquals(currencySelected, lst.get(0),"FAILED! Currency between BO and Before Login member site does not match, BO: "
+                + currencySelected + " Member Site: " + lst.get(0));
+        Assert.assertEquals(String.valueOf(rate), lst.get(2),"FAILED! Currency Rate between BO and Before Login member site does not match, BO: "
+                + rate + " Member Site: " + lst.get(2));
+
+        log("INFO: Executed completely");
+    }
+
+    @TestRails(id = "1660")
+    @Test (groups = {"regression1"})
+    @Parameters({"feMemberLoginId","feMemberLoginPwd","language","currency"})
+    public void BO_Operations_Before_Login_Management_1660(String feMemberLoginId, String feMemberLoginPwd, String language, String currency) throws Exception {
+        String type = "Header Menu";
+        log("@title: Validate Currency show correctly");
+        log("Step 1. Access Operations > Before Login Management");
+        BeforeLoginManagementPage page = backofficeHomePage.navigateBeforeLoginManagement();
+
+        log("Step 2. Filter with type = Header Menu for FairExchange");
+        page.filter(type,"FairExchange","", BOConstants.Operations.BannerManagement.POPUP_DDB_STATUS.get(0),true);
+        String domain = page.ddpDomain.getFirstSelectedOption();
+        log("Step 3. Get currency of current branch filter");
+        String languageSelected = page.ddpLanguage.getFirstSelectedOption();
+        memberLoginURL = "https://"+ domain + "/plus";
+        memberSOSUrl = "https://"+ domain;
+        log("Step 4. Login to member site");
+        loginMember(feMemberLoginId, feMemberLoginPwd, false, language, currency, true);
+        List<String> lst = landingPage.getBeforeLoginConfig(memberSOSUrl);
+
+        log("Verify. Validate language between BO site and Member site before login matched");
+        Assert.assertEquals(BOConstants.Operations.LANGUAGE.get(languageSelected), lst.get(1),"FAILED! Currency between BO and Before Login member site does not match, BO: "
+                + languageSelected + " Member Site: " + lst.get(1));
+
+        log("INFO: Executed completely");
     }
 }
---
+++ src/main/java/backoffice/utils/system/CurrencyManagementUtils.java
@@ -0,0 +1,33 @@
+package backoffice.utils.system;
+
+import com.paltech.constant.Configs;
+import com.paltech.driver.DriverManager;
+import com.paltech.utils.WSUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.util.Objects;
+
+import static baseTest.BaseCaseTest.environment;
+
+public class CurrencyManagementUtils {
+    public static Float getCurrencyRate(String fromCurrency) {
+        Float rate = null;
+        String api = String.format("%s/system-manager/web/sv/currency/getAll", environment.getBackofficeURL());
+        JSONArray jsonArray = WSUtils.getGETJSONArrayWithCookies(api, Configs.HEADER_JSON_CHARSET, DriverManager.getDriver().getCookies().toString(), Configs.HEADER_JSON);
+        if (Objects.nonNull(jsonArray)) {
+            if (jsonArray.length() > 0) {
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject orderObj = jsonArray.getJSONObject(i);
+                    if (orderObj.getString("currencyCode").equals(fromCurrency)) {
+                        rate = orderObj.getFloat("rate");
+                        return rate;
+                    }
+                }
+            }
+        }
+        return rate;
+    }
+}
+
+
+
