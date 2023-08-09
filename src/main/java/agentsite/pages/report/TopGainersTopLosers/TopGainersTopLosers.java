fb2f164eb08da3fd2ec919f717ea1cf98de50f51
Ethan Nguyen <Ethan.Nguyen@pal.net.vn>
2023-08-09 15:59:30 +0700

    Modify methods in TopGainersTopLosers

--- src/main/java/agentsite/pages/report/TopGainersTopLosers/TopGainersTopLosers.java
+++ src/main/java/agentsite/pages/report/TopGainersTopLosers/TopGainersTopLosers.java
@@ -5,9 +5,6 @@ import java.util.List;
 
 public class TopGainersTopLosers {
     public boolean isHeaderTableDisplayCorrect(String tableName){return false;}
-    public boolean isTopGainersHeaderTable(){return false;}
-    public boolean isTopLosersHeaderTable(){return false;}
-    public boolean isBigStakeHeaderTable(){return false;}
     public boolean isUserDisplayInBigStakeTableCorrect(String bigStake) {return false;}
     public boolean isUserDisplayInTableCorrect(String tableName, List<ArrayList<String>> lstWinLose) { return false;}
 }
