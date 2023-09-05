beefb84b0d739bdbb00009e8a6afd5180ed18741
Tim.Dang <tim.dang@pal.net.vn>
2023-09-05 15:36:26 +0700

    Rollback duration for v1.0.4

--- src/main/java/membersite/controls/DropDownBox.java
+++ src/main/java/membersite/controls/DropDownBox.java
@@ -207,7 +207,7 @@ public class DropDownBox extends BaseElement {
     }
 
     public WebElement waitForElementToBePresent(By by, int timeOutInSeconds) {
-        WebDriverWait wait = new WebDriverWait(DriverManager.getDriver(), Duration.ofSeconds(timeOutInSeconds));
+        WebDriverWait wait = new WebDriverWait(DriverManager.getDriver(), timeOutInSeconds);
         return wait.until(ExpectedConditions.presenceOfElementLocated(by));
     }
 
