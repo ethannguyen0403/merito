83ad838e5e54e5ccacbf133b40018aaa506009e8
Isabella Huynh <isabella.huynh@pal.net.vn>
2024-01-05 15:09:21 +0700

    Init proteus script implement test case 8: 4062, 4063,4064,4065,4066, 4059,4060

--- src/main/java/membersite/pages/proteus/ProteusHomePage.java
+++ src/main/java/membersite/pages/proteus/ProteusHomePage.java
@@ -3,11 +3,41 @@ package membersite.pages.proteus;
 import com.paltech.element.common.Label;
 import membersite.pages.HomePage;
 
+import static common.ProteusConstant.ASIAN_VIEW;
+import static common.ProteusConstant.EURO_VIEW;
+
 public class ProteusHomePage extends HomePage {
     public Label lblView = Label.xpath("//li[contains(@class,'view-mode')]/span");
+    public Label lblLoading = Label.xpath("//div[contains(@class,'loading-text')]/p");
     public ProteusHomePage(String types) {
         super(types);
     }
+
+    public void waitiFrameLoad(){
+        lblLoading.waitForControlInvisible(2,2);
+    }
+
+    public EuroViewPage selectEuroView(){
+        selectView(EURO_VIEW);
+        EuroViewPage euroViewPage = new EuroViewPage(this._type);
+        euroViewPage.waitContentLoad();
+        return euroViewPage;
+    }
+    public AsianViewPage selectAsianView(){
+        selectView(ASIAN_VIEW);
+        AsianViewPage asianViewPage = new AsianViewPage(this._type);
+        asianViewPage.waitContentLoad();
+        return asianViewPage;
+    }
+
+    private void selectView(String view){
+        // Display view
+        String currentView = lblView.getText();
+        if (currentView.equals(view))
+            lblView.click();
+    }
+
+
 
 
 }
