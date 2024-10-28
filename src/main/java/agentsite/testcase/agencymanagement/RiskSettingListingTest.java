3f95fb83b0f554a0027d5d20efd8c2cbdc513987
Ethan Nguyen <Ethan.Nguyen@pal.net.vn>
2024-10-28 14:13:12 +0700

    implement error console

--- src/main/java/agentsite/testcase/agencymanagement/RiskSettingListingTest.java
+++ src/main/java/agentsite/testcase/agencymanagement/RiskSettingListingTest.java
@@ -1,6 +1,7 @@
 package agentsite.testcase.agencymanagement;
 
 import agentsite.objects.agent.account.AccountInfo;
+import agentsite.pages.agentmanagement.CreditBalanceListingPage;
 import agentsite.pages.agentmanagement.RiskSettingListingPage;
 import agentsite.ultils.account.ProfileUtils;
 import agentsite.ultils.agencymanagement.DownLineListingUtils;
@@ -20,9 +21,10 @@ public class RiskSettingListingTest extends BaseCaseTest {
         //TODO: implement test for this case
         log("INFO: Executed completely");
     }
+
     @TestRails(id = "3641")
     @Test(groups = {"regression_credit"})
-    public void Agent_AM_Tax_Setting_Listing_3641() {
+    public void Agent_AM_Risk_Setting_Listing_3641() {
         log("@title: Validate  Risk Setting Listing UI display correct");
         log("Step 1. Navigate Agency Management > Risk Setting Listing");
         RiskSettingListingPage page = agentHomePage.navigateRiskSettingListingPage();
@@ -35,7 +37,7 @@ public class RiskSettingListingTest extends BaseCaseTest {
 
     @TestRails(id = "3642")
     @Test(groups = {"regression_credit"})
-    public void Agent_AM_Tax_Setting_Listing_3642() {
+    public void Agent_AM_Risk_Setting_Listing_3642() {
         log("@title: Validate  Risk Setting Listing UI display correct");
         log("Step 1. Navigate Agency Management > Risk Setting Listing");
         AccountInfo accountInfo = ProfileUtils.getProfile();
@@ -55,6 +57,15 @@ public class RiskSettingListingTest extends BaseCaseTest {
         log("INFO: Executed completely");
     }
 
+    @TestRails(id = "69449")
+    @Test(groups = {"http_request"})
+    public void Agent_AM_Risk_Setting_Listing_69449() {
+        log("@title: Validate there is no http responded error returned");
+        log("Step 1. Navigate Report >  Credit Balance Listing");
+        RiskSettingListingPage page = agentHomePage.navigateRiskSettingListingPage();
+        log("Verify 1: Validate there is no console error display");
+        Assert.assertTrue(hasHTTPRespondedOK(), "ERROR: There are some response request error returned");
+    }
 
 }
 
